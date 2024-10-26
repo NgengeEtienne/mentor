838a6b3562864ed8e9496b4b8d3c3f5154c05885
ngengeetienne <ngengeetienne@gmail.com>
2024-10-26 13:25:11 +0100

    mentor inherit cabriot2.1

--- MentorDashboard/middleware.py
+++ MentorDashboard/middleware.py
@@ -5,11 +5,11 @@ class BranchCompanyMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.user.is_authenticated:
            
-            # request.branch = Branch.objects.get(id=request.user.branch.id)
-            # request.company = Company.objects.get(id=request.user.branch.company_id)
+            request.branch = Branch.objects.get(id=request.user.branch.id)
+            request.company = Company.objects.get(id=request.user.branch.company_id)
 
-            request.company= Company.objects.get(id=1)
-            request.branch = Branch.objects.get(id=1)
+            # request.company= Company.objects.get(id=1)
+            # request.branch = Branch.objects.get(id=1)
         else:
             request.branch = Branch.objects.get(id=1)
             request.company = Company.objects.get(id=1)
