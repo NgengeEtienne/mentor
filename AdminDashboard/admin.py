6960783af5de64a903824930c246774d461b226f
ngengeetienne <ngengeetienne@gmail.com>
2024-10-30 03:52:58 +0100

    admin

--- AdminDashboard/admin.py
+++ AdminDashboard/admin.py
@@ -1,21 +1,109 @@
 from django.contrib import admin
-from .models import Kitchen,Menu, DishInfo, Meal, CuisineChoice, DietaryChoice, MealChoice, MealPlan, City,\
-    State, KitchenMenus,BulkOrders
+from .models import (
+    DietaryChoice,
+    CuisineChoice,
+    MealChoice,
+    State,
+    City,
+    Kitchen,
+    DishInfo,
+    Meal,
+    Menu,
+    KitchenMenus,
+    UserOrder,
+    BulkOrders,
+    MealPlan,
+)
 
-# Register your models here.
-admin.site.register(Kitchen)
-admin.site.register(Menu)
-admin.site.register(DishInfo)
-admin.site.register(Meal)
-admin.site.register(DietaryChoice)
-admin.site.register(CuisineChoice)
-admin.site.register(MealChoice)
+# Inline for selected dishes in Meal
+class DishInfoInline(admin.TabularInline):
+    model = Meal.selected_dishes.through  # Correctly referencing the through model
+    extra = 1  # Number of empty forms to display
+
+# Inline for Kitchen in KitchenMenus
+class KitchenMenuInline(admin.TabularInline):
+    model = KitchenMenus.kitchens.through  # Correctly referencing the through model
+    extra = 1  # Number of empty forms to display
+
+# Admin for DietaryChoice
+@admin.register(DietaryChoice)
+class DietaryChoiceAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+# Admin for CuisineChoice
+@admin.register(CuisineChoice)
+class CuisineChoiceAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+# Admin for MealChoice
+@admin.register(MealChoice)
+class MealChoiceAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+# Admin for State
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+# Admin for City
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state')
+
+# Admin for Kitchen
+@admin.register(Kitchen)
+class KitchenAdmin(admin.ModelAdmin):
+    list_display = ('kitchenName', 'kitchenAddress', 'kitchenState', 'is_delivery_available')
+    inlines = [KitchenMenuInline]  # Display menus for each kitchen
+
+# Admin for DishInfo
+@admin.register(DishInfo)
+class DishInfoAdmin(admin.ModelAdmin):
+    list_display = ('dish_name', 'dish_base_price', 'dish_availability_status')
+    inlines = [DishInfoInline]  # Display dish info in meals
+
+# Admin for Meal
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('meal_name', 'meal_base_price', 'meal_availability_status')
+    inlines = [DishInfoInline]  # Display selected dishes in the meal
+
+# Admin for Menu
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('menu_id', 'dish_name', 'meal_name', 'menu_price', 'menu_qty')
+
+# Admin for KitchenMenus
+@admin.register(KitchenMenus)
+class KitchenMenusAdmin(admin.ModelAdmin):
+    list_display = ('get_kitchens', 'get_kitchen_menus')
+
+    def get_kitchens(self, obj):
+        return ", ".join([kitchen.kitchenName for kitchen in obj.kitchens.all()])
+    get_kitchens.short_description = 'Kitchens'
+
+    def get_kitchen_menus(self, obj):
+        return ", ".join([menu.dish_name for menu in obj.kitchen_menus.all()])
+    get_kitchen_menus.short_description = 'Kitchen Menus'
+
 admin.site.register(MealPlan)
-# admin.site.register(CorperateMealPlan)
-admin.site.register(State)
-admin.site.register(City)
-admin.site.register(KitchenMenus)
-admin.site.register(BulkOrders)
 
+# Admin for BulkOrders
+@admin.register(BulkOrders)
+class BulkOrdersAdmin(admin.ModelAdmin):
+    list_display = ('bulk_order_name', 'company', 'branch', 'bulk_order_start_date', 'bulk_order_end_date')
+    search_fields = ('bulk_order_name', 'bulk_order_description')
+    list_filter = ('company', 'branch')
 
+# Inline for Meal in UserOrder
+class MealInline(admin.TabularInline):
+    model = UserOrder.Meal.through  # Correctly referencing the through model
+    extra = 1  # Number of empty forms to display
 
+# Admin for UserOrder
+@admin.register(UserOrder)
+class UserOrderAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'user_id', 'order_date', 'total_price', 'order_status')
+    search_fields = ('user_id', 'order_id')
+    list_filter = ('order_status', 'order_type', 'payment_status')
+    inlines = [MealInline]  # Display meals in user orders, not DishInfoInline
--- MentorDashboard/admin.py
+++ MentorDashboard/admin.py
@@ -3,6 +3,38 @@ from django.contrib import admin
 from .models import DeliveryAddress, MealDelivery, Notification
 
 
-admin.site.register(DeliveryAddress)
-admin.site.register(MealDelivery)
-admin.site.register(Notification)
+from django.contrib import admin
+from .models import DeliveryAddress, MealDelivery, Notification
+
+# Inline for MealDelivery
+from django.contrib import admin
+from .models import DeliveryAddress, MealDelivery, Notification
+
+# Inline for MealDelivery
+class MealDeliveryInline(admin.TabularInline):
+    model = MealDelivery
+    extra = 1  # Number of empty forms to display
+    fields = ('bulk_order', 'meal_type', 'quantity', 'date', 'status')
+
+# Inline for Notification
+class NotificationInline(admin.TabularInline):
+    model = Notification
+    extra = 1  # Number of empty forms to display
+    fields = ('message',)
+
+# Admin for DeliveryAddress
+@admin.register(DeliveryAddress)
+class DeliveryAddressAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address_line_1', 'city', 'state', 'pin_code', 'default_address')
+    inlines = [MealDeliveryInline]  # Only include MealDelivery here
+
+# Admin for MealDelivery
+@admin.register(MealDelivery)
+class MealDeliveryAdmin(admin.ModelAdmin):
+    list_display = ('bulk_order', 'delivery_address', 'meal_type', 'quantity', 'date', 'status', 'created_at')
+    inlines = [NotificationInline]  # Include Notification inline here
+
+# Admin for Notification
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('delivery', 'message', 'branch', 'company', 'created_at')
